3d1f71d172bc71e22ed7f68990983563a37eb4f8
cyber <cybery0gi@cybers-MBP.lan>
2024-11-04 09:16:39 -0800

    implementing basic shiksha types

--- shiksha/types.go
+++ shiksha/types.go
@@ -1,8 +1,8 @@
 package shiksha
 
-// karneeyam: Devanagari transliterations for sanskrit comments is currently unsupported with nvim. Figure this out someday.
+// karneeyam: Devanagari transliterations for Sanskrit comments are currently unsupported with nvim. Figure this out someday.
 
-// Svara type as an enum
+// VedicSvara represents the tonal accent in Vedic Sanskrit (e.g., Anudaatta, Udaatta)
 type VedicSvara int
 
 const (
@@ -12,8 +12,7 @@ const (
     DeerghaSvarita
 )
 
-// String method to get a readable string representation of Svara
-func (s Svara) String() string {
+func (s VedicSvara) String() string {
     switch s {
     case Anudaatta:
         return "anudaatta"
@@ -28,81 +27,69 @@ func (s Svara) String() string {
     }
 }
 
-// Akshara struct represents a Sanskrit character
-type Akshara struct {
-    Unicode    string `json:"unicode"`
-    Devanagari string `json:"devanagari"`
-    Roman      string `json:"roman"`
-}
-
-/***
-// Token struct represents a Sanskrit phonetic unit with Svara, Matra, and Aksharas
-type Token struct {
-    Svara     Svara     `json:"svara"`
-    Matra     int       `json:"matra"`
-    Aksharas  []Akshara `json:"aksharas"`
-}
-***/
-
-// || arthavat padaanaam samoohaH vaakyam ||
-// Vaakya represents an array of padas that convey meaning.
-type Vaakya []Pada
-
-// Pada represents a group of Varnas
-// ||varNaanaam samoohaH padam||
-type Pada []Token
-
-// || svatantradhvaniH varNaH bhavati ||
-//  Independent sounds are Varnas
-
-
 // || na ksheeyate na ksharati iti vaa aksharam ||
 // That which neither diminishes nor decays is called an aksharam
+// Akshara represents a basic Sanskrit character or sound unit
+type Akshara struct {
+    Unicode    string `json:"unicode"`    // Unicode representation
+    Devanagari string `json:"devanagari"` // Devanagari script representation
+    Roman      string `json:"roman"`      // Roman transliteration
+}
 
-
-// || svayam raajante iti svaraaH ||
-// a svara is that which shines by itself is called a svara
-
-
-
+// || svatantradhvaniH varNaH bhavati ||
+// Independent sounds are Varnas
+// Varna represents an independent phonetic unit in Sanskrit
+type Varna struct {
+    Akshara Akshara   `json:"akshara"` // The core sound unit
+    Svara   VedicSvara `json:"svara"`   // The tonal accent of the Varna
+}
 
 // || vyanjanaani anvag bhavati iti vyanjanam ityaaha patanjali ||
 // || svaradharmam anugacchati iti vyanjanam || 
 // As per Patanjali - that which is dependent on a vowel is called as "Vyanjana" (Consonant)
 // Vyanjanas follow the nature of svaras 
-
+// Vyanjana represents a consonant that depends on a Svara (vowel) for pronunciation
+type Vyanjana struct {
+    Varna Varna `json:"varna"` // The underlying Varna representing this consonant
+}
 
 // ayogavaahaaH - letters which are not termed or classified under svaras or vyanjanas but still seen in usage are referred to as ayogavaahaa
+// Ayogavaaha represents auxiliary sounds that are neither Svaras nor Vyanjanas
+type Ayogavaaha struct {
+    Sound string `json:"sound"` // A sound like a nasal or breath that’s not a vowel or consonant
+}
 
-// ANUSVARA
 // || svaram anuprayujyate iti anusvaaraH || 
 // || anusvaaraH svaraatparaH prayujyate ||
-// 
+// Anusvara represents a nasal sound used as an extension of Svara
+type Anusvara struct {
+    Varna Varna `json:"varna"` // The underlying Varna followed by nasalization
+}
 
-// VISARGA 
 // || vividhataam srjati iti visargaH ||
 // That which manifests itself in many ways is Visarga
+// Visarga represents an aspirate sound (ḥ), occurring as a modification of a Svara
+type Visarga struct {
+    Varna Varna `json:"varna"` // The underlying Varna with an added aspirate sound
+}
 
- 
-// JIHVAAMOOLIYA
 // || kakaabhyaam praag ardhavisargasadrshaH jihvamooliyaH ityucyate ||
+// Jihvamuliya is a special variant of Visarga, pronounced before velar sounds
+type Jihvamuliya struct {
+    Varna Varna `json:"varna"` // The underlying Varna with a half-visarga sound before velars
+}
 
-
-// UPADHMAANIYA 
-
+// Upadhmaaniya is a special variant of Visarga, pronounced before labials
+type Upadhmaaniya struct {
+    Varna Varna `json:"varna"` // The underlying Varna with a half-visarga sound before labials
+}
 
 // yamaaH
 // If any of the 4 letters of a varga comes in front of the 5th varna - a similar letter is added as per shiksha called "yamavarna"
 // i.e. agni - g and n have come together - so agni is pronounced as "aggni" - this second `g` is a yama varna
 // 4 yama varnas in rig veda pratisakhya
-
-
-
-
-
-
-
-
-
-
- 
+// Yama represents a duplicated consonant sound due to sandhi (phonetic combination) rules
+type Yama struct {
+    Primary   Varna `json:"primary"`   // The original Varna
+    Duplicate Varna `json:"duplicate"` // The duplicated Varna following sandhi rules
+}
